89fbe79b39da56c56029c98b1268fb04723190dd
Melissa Barrios <melissa.blecona@gmail.com>
2022-06-30 14:53:49 -0500

    tarea finalizada

---
+++ index.js
@@ -0,0 +1,11 @@
+let name = 'Melissa';
+let number = 32;
+let desarrolladora = true;
+let dateOfBirth = new Date("dec, 11, 1990");
+let libro = {
+    titulo: 'Caballo de Troya',
+    autor: 'Juan Jose Benitez',
+    url :"https://www.amazon.com.mx/Jerusal%C3%A9n-Caballo-Troya-Nueva-edic/dp/6070709543/ref=sr_1_1?keywords=caballo+de+troya+1&qid=1656617833&sprefix=caballo+%2Caps%2C975&sr=8-1"
+}
+
+
